4ca63828784ba6d8821b6adeaa0f95a7bfea9689
bublik <bublik.script@gmail.com>
2024-05-18 15:46:03 +0300

    Update app descriptions in layout and manifest files
    - Shortened description in `app/layout.tsx`
    - Modified short_name and description in `public/manifest.json`

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 export const metadata: Metadata = {
   title: 'Party Games',
-  description:
-    'Party Games: A collection of cool and interactive games designed for party, friends, and fun.',
+  description: 'A collection of cool and interactive games designed for party, friends, and fun.',
   category: 'Games',
   keywords: [
     'party',
